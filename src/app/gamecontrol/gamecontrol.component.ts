b79d7a9d45f9cd90eabf9b292da457b2455b54af
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-06-06 14:23:49 -0700

    interval working

--- src/app/gamecontrol/gamecontrol.component.ts
+++ src/app/gamecontrol/gamecontrol.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gamecontrol.component.css']
 })
 export class GamecontrolComponent implements OnInit {
+  counter: number = 0;
+  interval;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame(){
+    this.interval = setInterval(() => this.counter++, 1000);
+  }
+
+  onEndGame(){
+    clearInterval(this.interval);
   }
 
 }
